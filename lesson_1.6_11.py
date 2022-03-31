a646da04e7b0379e0572b9eec4f13514a5a51ad7
Artem Glebezdin <artem_glebezdin@mail.ru>
2022-03-31 12:21:06 +0300

    Задание для самопроверки. Глава 1.6 / 11 часть

---
+++ lesson_1.6_11.py
@@ -0,0 +1,50 @@
+#
+# Glebezdin Artem
+# lesson 1.6_8
+# v1.0
+#
+
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+import time
+
+
+try:
+    # Ссылка 1, результат True
+    link_1 = "http://suninjuly.github.io/registration1.html"
+    # Ссылка 2, результат False
+    link_2 = "http://suninjuly.github.io/registration2.html"
+
+    # запускаем драйвер, передаем ссылку на продукт
+    browser = webdriver.Chrome()
+    browser.get(link_1)
+
+    # Заполнение полей тестовой информацией
+    input1 = browser.find_element(By.CSS_SELECTOR, "[placeholder='Input your first name']")
+    input1.send_keys("Artem")
+    input2 = browser.find_element(By.CSS_SELECTOR, "[placeholder='Input your last name']")
+    input2.send_keys("Ivanov")
+    input3 = browser.find_element(By.CSS_SELECTOR, "[placeholder='Input your email']")
+    input3.send_keys("test@test.com")
+
+    # нажимаем кнопку "Submit"
+    button = browser.find_element(By.CLASS_NAME, "btn.btn-default")
+    button.click()
+
+    # Проверяем, что смогли зарегистрироваться
+    # ждем загрузки страницы
+    time.sleep(3)
+
+    # находим элемент, содержащий текст
+    welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+    # записываем в переменную welcome_text текст из элемента welcome_text_elt
+    welcome_text = welcome_text_elt.text
+
+    # Проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+    assert "Congratulations! You have successfully registered!" == welcome_text
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(5)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
